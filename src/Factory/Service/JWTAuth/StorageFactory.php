a7790783248e47ec457e0b119809bf577c9b610b
J Ngo <ngonam22@gmail.com>
2020-01-16 17:16:23 +0700

    append the JWT skeleton for ZF

---
+++ src/Factory/Service/JWTAuth/StorageFactory.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 2020-01-16
+ * Time: 16:59
+ */
+
+namespace StCommonService\Factory\Service\JWTAuth;
+
+
+class StorageFactory
+{
+
+}
---
+++ src/Service/JWTAuth/AuthenticationService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 2020-01-16
+ * Time: 16:51
+ */
+
+namespace StCommonService\Service\JWTAuth;
+
+
+class AuthenticationService
+{
+
+}
